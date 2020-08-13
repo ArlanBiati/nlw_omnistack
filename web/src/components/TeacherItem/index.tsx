0eed3e26c1009958df9fd9f1baa815319016c572
ArlanBiati <arlangustavo@hotmail.com>
2020-08-13 12:36:32 -0300

    Chore: Finalizado Frontend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,34 +3,56 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
+
+export interface Teacher {
+    
+    id: number,
+    name: string,
+    avatar: string,
+    subject: string,
+    bio: string,
+    whatsapp: string,
+    cost: number
+    
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
 
 
-function TeacherItem() {
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/43690080?s=460&u=671312ce6fec36b5d95ffe46c625402c87acec0b&v=4" alt="Arlan Biati"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Arlan Biati</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis veniam officiis, unde doloremque nesciunt minus! Dolores ullam ea quo ipsum, dolor nisi minus reprehenderit qui, illum ipsa sunt labore in!
-                <br></br>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque illo illum, quam perferendis accusantium deserunt hic vero eius error magnam? Ratione aliquid iure facere ullam porro adipisci deleniti enim.
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 20,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blanck" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
 
         </article>
